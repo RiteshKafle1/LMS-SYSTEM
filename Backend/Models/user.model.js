05b6f0e6977670e6734a9ce539eec983c037181f
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-06-15 22:34:41 +0545

    a user model created success.

---
+++ Backend/Models/user.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+    },
+    imagePublicId: {
+      type: String,
+    },
+    enrolledCourses: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+const userModel = mongoose.models.User || mongoose.model("User", userSchema);
+module.exports = userModel;
