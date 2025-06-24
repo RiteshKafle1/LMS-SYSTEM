042a58e81cc34410edc4ab307baa092ffdb9c80c
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-06-24 08:18:08 +0545

    fetching the user profile.

--- Backend/Controller/user.controller.js
+++ Backend/Controller/user.controller.js
@@ -2,6 +2,7 @@ const userModel = require("../Models/user.model");
 const bcrypt = require("bcrypt");
 const validator = require("validator");
 const generateToken = require("../utils/generate.token");
+const { use } = require("bcrypt/promises");
 
 const registerUser = async (req, res) => {
   try {
@@ -10,21 +11,30 @@ const registerUser = async (req, res) => {
     const user = await userModel.findOne({ email });
 
     if (!fullName || !email || !password)
-      return res.json({ error: true, message: "No empty fields allowed" });
+      return res
+        .status(400)
+        .json({ error: true, message: "No empty fields allowed" });
 
     if (!validator.isEmail(email))
-      return res.json({ error: true, message: "Please enter a valid email" });
+      return res
+        .status(400)
+        .json({ error: true, message: "Please enter a valid email" });
 
     if (!validator.isStrongPassword(password))
-      return res.json({ error: true, message: "Password too weak" });
+      return res
+        .status(400)
+        .json({ error: true, message: "Password too weak" });
 
     if (!validator.isAlphanumeric(fullName))
-      return res.json({
+      return res.status(400).json({
         error: true,
         message: "Username should contain letters and numbers.",
       });
 
-    if (user) return res.json({ error: true, message: "User Already exists" });
+    if (user)
+      return res
+        .status(400)
+        .json({ error: true, message: "User Already exists" });
 
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(password, salt);
@@ -39,7 +49,7 @@ const registerUser = async (req, res) => {
     return res.status(201).json({
       error: false,
       message: "User Created Successfully",
-      fullName: newUser.fullName,
+      fullName: newUser.name,
       email: newUser.email,
     });
   } catch (error) {
@@ -55,10 +65,14 @@ const loginUser = async (req, res) => {
     const { email, password } = req.body;
 
     if (!email || !password)
-      return res.json({ error: true, message: "NO empty fields allowed" });
+      return res
+        .status(400)
+        .json({ error: true, message: "No empty fields allowed" });
     const user = await userModel.findOne({ email });
     if (!user)
-      return res.status(400).json({ error: true, message: "Incorrect email or password." });
+      return res
+        .status(400)
+        .json({ error: true, message: "Incorrect email or password." });
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch)
@@ -69,10 +83,29 @@ const loginUser = async (req, res) => {
     const token = generateToken(user._id);
     return res
       .status(200)
-      .json({ error: false, message: "Login Success.", token });
+      .json({ error: false, message: "Login Success.", token, user });
   } catch (error) {
     console.log("Error in logging user", error);
     return res.status(500).json({ error: true, message: "Login Failed" });
   }
 };
-module.exports = { registerUser, loginUser };
+
+const getUserProfile = async (req, res) => {
+  try {
+    const userId=req.user.userId;
+    const user = await userModel.findById(userId).select("-password");
+    if (!user)
+      return res
+        .status(400)
+        .json({ error: true, message: "Profile Not Found" });
+      
+
+    return res.status(200).json({ error: false, user });
+  } catch (error) {
+    console.log("Error in fetching userprofile");
+    return res
+      .status(500)
+      .json({ error: true, message: "Failed to load user" });
+  }
+};
+module.exports = { registerUser, loginUser, getUserProfile };
